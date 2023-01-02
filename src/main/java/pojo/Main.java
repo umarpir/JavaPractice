793cb1d893ba6c93065d068c727e18abaa30267e
umar, pirmahomed <umarpirmahomed786@gmail.com>
2023-01-02 02:14:21 +0000

    POJO: Bean example

---
+++ src/main/java/pojo/Main.java
@@ -0,0 +1,7 @@
+package main.java.pojo;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/pojo/Student.java
@@ -0,0 +1,24 @@
+package main.java.pojo;
+
+public class Student {
+    
+    String id;
+    String name;
+    String dateOfBirth;
+    String classList;
+    
+    public Student(String id, String name, String dateOfBirth, String classList) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.classList = classList;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [id=" + id + ", name=" + name + ", dateOfBirth=" + dateOfBirth + ", classList=" + classList
+                + "]";
+    }
+    
+    
+}
