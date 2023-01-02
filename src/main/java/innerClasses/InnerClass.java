ab07cadc330809a56482ccd042e31d39b45c85ad
umar, pirmahomed <umarpirmahomed786@gmail.com>
2023-01-02 22:20:44 +0000

    InnerClasses: Refactor for Gear example

--- src/main/java/innerClasses/InnerClass.java
+++ src/main/java/innerClasses/InnerClass.java
@@ -4,9 +4,7 @@ public class InnerClass {
 
     public static void main(String[] args) {
         Gearbox maclaren = new Gearbox(6);
-        maclaren.addGear(1,5.3);
-        maclaren.addGear(2,10.3);
-        maclaren.addGear(3,15.9);
+
 
         maclaren.operateClutch_(true);
         maclaren.changeGear(1);
